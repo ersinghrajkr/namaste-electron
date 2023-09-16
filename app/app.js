8be6ddd1642908c4b3afa160f103e0a7e6d3b174
ersinghrajkr <rkumar@otalio.com>
2023-09-16 11:46:59 +0530

    Worked on Todo App, Adding to do and clearing todo list

--- app/app.js
+++ app/app.js
@@ -1,28 +1,86 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require("path");
-const ffmpeg = require('fluent-ffmpeg');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 
-let mainWindow;
+let mainWindow, addWindow;
 
 app.on('ready', () => {
     console.log("App is now ready!");
-    mainWindow = new BrowserWindow({ 
+    mainWindow = new BrowserWindow({
+        title: 'Todo App',
         webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            preload: path.join(__dirname, "preload.js"),
-          },
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
     })
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    const mainMenuTemplate = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenuTemplate)
+
+
+    mainWindow.on('closed', () => {
+        // On closer of main windown, quit app completely
+        app.quit()
+    })
 })
 
-ipcMain.on('video:submit', (event, videoFilePath)=>{
-    console.log(1);
-    ffmpeg.ffprobe(videoFilePath, (error, metadata) => {
-        if(error) {
-            throw error
-        }
-        console.log('Video Duration is: ', metadata.format.duration)
-        event.sender.send("video:metadata", metadata.format.duration) 
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 350,
+        height: 300,
+        title: 'Add new Todo',
+        webPreferences: { nodeIntegration: true, contextIsolation: false },
     })
+    addWindow.loadURL(`file://${__dirname}/addTodo.html`);
+    addWindow.on("closed", () => (addWindow = null));
+}
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:addToList', todo);
+    console.log(`${todo} added!`)
+    addWindow.close()
 })
+
+const menuTemplate = [{
+    label: 'File', submenu: [
+        {
+            label: 'New Todo', click() { createAddWindow() },
+        },
+        {
+            label: 'Clear Todos',
+            click() {
+                mainWindow.webContents.send('todo:clear');
+            }
+        },
+        {
+            label: 'Exit',
+            accelerator: (() => {
+                if (process.platform === "darwin") {
+                    return 'Cmd+Q'
+                } else {
+                    return 'Ctrl + Q';
+                }
+            })(),
+            click() { app.quit() }
+        }]
+}]
+
+if (process.platform === "darwin") {
+    menuTemplate.unshift({ label: "" });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    // Add developer tools item to right-click menu in development mode only
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Developer Tools',
+                accelerator: process.platform === "darwin" ? 'Cmd+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+
+    })
+}
